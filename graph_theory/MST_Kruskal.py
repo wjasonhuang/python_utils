9333dc9e37bd88e955f45a5005a89fe8e91dcd64
wjasonhuang <wjasonhuang@users.noreply.github.com>
2019-11-03 09:53:05 -0500

    Update MST_Kruskal.py

--- graph_theory/MST_Kruskal.py
+++ graph_theory/MST_Kruskal.py
@@ -3,7 +3,7 @@ Minimum Spanning Tree Kruskal's Algorithm O(|E|log|V|)
 https://en.wikipedia.org/wiki/Kruskal%27s_algorithm
 '''
 
-from Union_Find_Set import UnionFindSet
+from union_find_set import UnionFindSet
 
 def MSTKruskal(n, e):
     # vertex: 0..n-1, e: list of [v1, v2, c] edge between v1 and v2 at cost c
