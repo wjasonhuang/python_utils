39b233c340ccc8f7b70f927b8a9b50c3d53c54c9
wjasonhuang <wjasonhuang@users.noreply.github.com>
2019-11-07 22:19:57 -0500

    Update lib_threading.py

--- concurrency/lib_threading.py
+++ concurrency/lib_threading.py
@@ -1,37 +1,88 @@
+'''
+class threading.Thread(group=None, target=None, name=None, args=(), kwargs={}, *, daemon=None)
+    start()
+    join(timeout=None)
+    is_alive()
+
+class threading.Lock
+    acquire(blocking=True, timeout=-1)
+    * Acquire a lock, blocking or non-blocking.
+    
+    release()
+    * Release a lock. This can be called from any thread, not only the thread which has acquired the lock.
+
+class threading.Semaphore(value=1)
+    The optional argument gives the initial value for the internal counter; it defaults to 1. 
+
+    acquire(blocking=True, timeout=None)
+    * If the internal counter is larger than zero on entry, decrement it by one.
+    * If the internal counter is zero on entry, block until awoken by a call to release().
+    
+    release()
+    * Release a semaphore, incrementing the internal counter by one.
+    * When it was zero on entry and another thread is waiting, wake up that thread.
+
+class threading.BoundedSemaphore(value=1)
+    A bounded semaphore checks to make sure its current value doesn’t exceed its initial value.
+    If it does, ValueError is raised.
+'''
+
 import time
-from threading import Thread, Lock
+from threading import Thread, Lock, Semaphore
 
-def calc(t_id, sleep_time):
-    print(f'Thread {t_id}: sleep {sleep_time} second(s)')
+def calc(t_id, sleep_time, output_lock):
+    with output_lock:
+        print(f'Thread {t_id}: sleep {round(sleep_time, 2)} second(s)')
     time.sleep(sleep_time)
-    print(f'Thread {t_id}: finished')
+    with output_lock:
+        print(f'Thread {t_id}: finished')
 
-def calc_lock(t_id, sleep_time):
-    global lock
+def calc_lock(t_id, sleep_time, sleep_lock):
     '''
         with lock:
         lock.acquire()
         lock.release()
     '''
-    with lock:
-        print(f'Thread {t_id}: sleep {sleep_time} second(s)')
+    with sleep_lock:
+        print(f'Thread {t_id}: sleep {round(sleep_time, 2)} second(s)')
         time.sleep(sleep_time)
         print(f'Thread {t_id}: finished')
- 
-n= 4
-sleep_time = [i / 2 for i in reversed(range(n))]
+
+def calc_n_times(t_id, sleep_time, output_lock, semaphore):
+    with output_lock:
+        print(f'Thread {t_id}: sleep {round(sleep_time, 2)} second(s)')
+    time.sleep(sleep_time)
+    with output_lock:
+        print(f'Thread {t_id}: finished')
+    semaphore.release()
+
+n= 9
+sleep_time = [i / 9 for i in reversed(range(n))]
+output_lock = Lock()
 
 print('----- No Lock -----')
 now = time.time()
-threads = [Thread(target=calc, args=(i, sleep_time[i])) for i in range(n)]
+threads = [Thread(target=calc, args=(i, sleep_time[i], output_lock)) for i in range(n)]
 for t in threads: t.start()
 for t in threads: t.join()
 print(f'Total time: {round(time.time() - now, 2)} second(s)\n')
 
 print('----- Lock -----')
 now = time.time()
-lock = Lock()
-threads = [Thread(target=calc_lock, args=(i, sleep_time[i])) for i in range(n)]
+sleep_lock = Lock()
+threads = [Thread(target=calc_lock, args=(i, sleep_time[i], sleep_lock)) for i in range(n)]
 for t in threads: t.start()
+for t in threads: t.join()
+print(f'Total time: {round(time.time() - now, 2)} second(s)\n')
+
+print('----- Semaphore -----')
+now = time.time()
+semaphore = Semaphore(3) # max 3 threads each time
+threads = []
+for i in range(n):
+    semaphore.acquire()
+    t = Thread(target=calc_n_times, args=(i, sleep_time[i], output_lock, semaphore))
+    t.start()
+    threads.append(t)
 for t in threads: t.join()
 print(f'Total time: {round(time.time() - now, 2)} second(s)\n')
