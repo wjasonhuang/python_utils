fe689c8c7c75bfd528377325797ae56a1036a1cf
wjasonhuang <wjasonhuang@users.noreply.github.com>
2019-11-09 23:52:42 -0500

    Update LeetCode_1242.py

--- concurrency/LeetCode_1242.py
+++ concurrency/LeetCode_1242.py
@@ -9,38 +9,6 @@
 #        :rtype List[str]
 #        """
 
-from threading import Thread, Lock
-
-class Solution:
-    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
-        
-        def crawler(startUrl):
-            nonlocal mutex
-            
-            startHost = startUrl.split('/')[2]
-            urlList = htmlParser.getUrls(startUrl)
-            with mutex:
-                for url in urlList:
-                    if url in visited or url.split('/')[2] != startHost: continue
-                    visited.add(url)
-                    q.append(url)
-        
-        q, visited = collections.deque([startUrl]), set([startUrl])
-        n, mutex = 8, Lock()
-        
-        while q:
-            threads = []
-            while len(threads) < n and q:
-                newUrl = q.popleft()
-                newThread = Thread(target=crawler, args=[newUrl])
-                threads.append(newThread)
-
-            for t in threads: t.start()
-            for t in threads: t.join()
-        
-        return list(visited)
-
-
 from threading import Lock
 from concurrent.futures import ThreadPoolExecutor
 
@@ -63,4 +31,39 @@ class Solution:
             with ThreadPoolExecutor() as executor:
                 executor.map(crawler, q)
             q = newq
+        return list(visited)
+
+
+# producer-consumer using queue
+from threading import Thread, Lock
+from queue import Queue
+
+class Solution:
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+        
+        def crawler(lock):
+            while True:
+                startUrl = q.get()
+                if startUrl == None: break
+                startHost = startUrl.split('/')[2]
+                urlList = htmlParser.getUrls(startUrl)
+                with lock:
+                    for url in urlList:
+                        if url in visited or url.split('/')[2] != startHost: continue
+                        visited.add(url)
+                        q.put(url)
+                q.task_done()
+        
+        n = 10
+        threads, visited = [], set([startUrl])
+        lock, q = Lock(), Queue()
+        q.put(startUrl)
+        
+        for _ in range(n):
+            t = Thread(target=crawler, args=(lock,))
+            t.start()
+            threads.append(t)
+        q.join()
+        for _ in range(n): q.put(None)
+        for t in threads: t.join()
         return list(visited)
