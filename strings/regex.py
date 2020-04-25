54dadb1726088f82228b5604bec9bbee6c6874b5
wjasonhuang <wjasonhuang@users.noreply.github.com>
2020-04-25 14:55:26 -0400

    Update regex.py

--- strings/regex.py
+++ strings/regex.py
@@ -2,10 +2,10 @@
 https://docs.python.org/3/howto/regex.html
 https://github.com/ziishaned/learn-regex
 
-re.findall(A, B)
-re.search(A, B)
-re.split(A, B)
-re.sub(A, B, C)
+re.findall(A, B)    Matches all instances of an expression A in a string B and returns them in a list.
+re.search(A, B)     Matches the first instance of an expression A in a string B, and returns it as a re match object.
+re.split(A, B)      Split a string B into a list using the delimiter A.
+re.sub(A, B, C)     Replace A with B in the string C.
 
 Regex Flags
 re.IGNORECASE   ignore case.
@@ -21,3 +21,7 @@ with open('new.txt', 'r', encoding='utf-8') as f:
         ret = re.findall('the', line, re.IGNORECASE)
         total += len(ret)
     print(total)
+
+s = 'hello world!'
+print(re.split('l', s))
+print(re.sub('l+', '$', s))
