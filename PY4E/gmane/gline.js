48a053ccadbd5d983c64ef08235ff9bad930d7d2
wjasonhuang <wjasonhuang@users.noreply.github.com>
2020-04-25 21:36:00 -0400

    PY4E

---
+++ PY4E/gmane/gline.js
@@ -0,0 +1,5 @@
+gline = [ ['Month','umich.edu','indiana.edu','berkeley.edu','ucdavis.edu','uct.ac.za','mac.com','gmail.com','yale.edu','cam.ac.uk','columbia.edu'],
+['2005-12',57,12,12,11,14,12,10,5,7,13],
+['2006-01',114,32,33,37,27,14,23,11,19,28],
+['2006-02',111,29,25,16,21,34,23,38,25,9]
+];
