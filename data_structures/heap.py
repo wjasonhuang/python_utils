47a76fa176bb75466a694ccbcf0e7f0b8007703e
wjasonhuang <wjasonhuang@users.noreply.github.com>
2020-05-03 12:05:23 -0400

    Update heap.py

--- data_structures/heap.py
+++ data_structures/heap.py
@@ -5,25 +5,35 @@ Heap with remove operation via heapq + dict
 import heapq
 
 
-class MinHeap():
-    def __init__(self):
-        self.heap = []
+class MinHeap:
+    def __init__(self, heap=[]):
+        self.heap = heap
+        self.size = len(heap)
+        heapq.heapify(self.heap)
         self.to_remove = dict()
-        self.size = 0
 
     def heappush(self, val):
         heapq.heappush(self.heap, val)
         self.size += 1
 
     def heappop(self):  # only return when heap not empty
-        while self.heap:
-            ret = heapq.heappop(self.heap)
-            if self.to_remove.get(ret, 0) > 0:
-                self.to_remove[ret] -= 1
-                continue
+        if self.size > 0:
+            self.__update()
             self.size -= 1
-            return ret
+            return heapq.heappop(self.heap)
+
+    def heapmin(self):
+        if self.size > 0:
+            self.__update()
+            return self.heap[0]
 
     def remove(self, val):
         self.to_remove[val] = self.to_remove.get(val, 0) + 1
         self.size -= 1
+
+    def empty(self):
+        return self.size == 0
+
+    def __update(self):
+        while self.heap and self.to_remove.get(self.heap[0], 0) > 0:
+            self.to_remove[heapq.heappop(self.heap)] -= 1
