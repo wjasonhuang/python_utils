39171bb0140101b7e9c95798b379e699f5365698
wjasonhuang <54944790+wjasonhuang@users.noreply.github.com>
2020-05-17 14:45:33 -0400

    Update LeetCode_1195.py

--- concurrency/LeetCode_1195.py
+++ concurrency/LeetCode_1195.py
@@ -1,14 +1,13 @@
+from threading import Thread, Condition
 from typing import Callable
-from threading import Thread
 
-from threading import Condition
 
 class FizzBuzz:
     def __init__(self, n: int):
         self.n = n
         self.cur = 1
         self.cv = Condition()
-    
+
     def run(self, wait_cond, func, arg=0):
         while True:
             self.cv.acquire()
@@ -26,18 +25,19 @@ class FizzBuzz:
     # printFizz() outputs "fizz"
     def fizz(self, printFizz: 'Callable[[], None]') -> None:
         self.run(lambda x: x <= self.n and (x % 3 != 0 or x % 15 == 0), printFizz)
-    
+
     # printBuzz() outputs "buzz"
     def buzz(self, printBuzz: 'Callable[[], None]') -> None:
         self.run(lambda x: x <= self.n and (x % 5 != 0 or x % 15 == 0), printBuzz)
-    
+
     # printFizzBuzz() outputs "fizzbuzz"
     def fizzbuzz(self, printFizzBuzz: 'Callable[[], None]') -> None:
         self.run(lambda x: x <= self.n and x % 15 != 0, printFizzBuzz)
-    
+
     # printNumber(x) outputs "x", where x is an integer.
     def number(self, printNumber: 'Callable[[int], None]') -> None:
         self.run(lambda x: x <= self.n and (x % 3 == 0 or x % 5 == 0), printNumber, 1)
+
 
 threads = []
 new = FizzBuzz(15)
