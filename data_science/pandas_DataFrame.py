7c7173c786c0647852b98cda94c080eea04525c4
wjasonhuang <wjasonhuang@users.noreply.github.com>
2020-06-06 00:41:58 -0400

    Create pandas_DataFrame.py

---
+++ data_science/pandas_DataFrame.py
@@ -0,0 +1,27 @@
+"""
+https://pandas.pydata.org/docs/reference/frame.html
+
+DataFrame.index         The index (row labels) of the DataFrame
+DataFrame.columns       The column labels of the DataFrame
+DataFrame.dtypes        Return the dtypes in the DataFrame
+DataFrame.values        Return a Numpy representation of the DataFrame
+DataFrame.axes          [index, columns]
+DataFrame.ndim          Return dimensions, 1 for array, 2 for matrix
+DataFrame.size          len(index) * len(columns)
+DataFrame.shape         Return a tuple representing the dimensionality of the DataFrame
+DataFrame.empty         Indicator whether DataFrame is empty
+
+"""
+
+import pandas as pd
+
+x = {'id': 'x', 'a': 11, 'b': 12, 'c': 13}
+y = {'id': 'y', 'a': 21, 'b': 22, 'c': 23}
+df = pd.DataFrame([x, y]).set_index('id')
+print(df)
+
+# assign value to cell
+df.at['x', 'a'] = 41
+df.loc['y']['b'] = 42
+df.iloc[1][2] = 43
+print(df)
