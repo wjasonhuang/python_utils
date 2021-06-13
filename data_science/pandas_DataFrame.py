5264aa90615cb7fe35937b3f3cf66b5cd0d49c2b
wjasonhuang <54944790+wjasonhuang@users.noreply.github.com>
2021-06-13 11:52:01 -0400

    Update pandas_DataFrame.py

--- data_science/pandas_DataFrame.py
+++ data_science/pandas_DataFrame.py
@@ -1,44 +1,38 @@
 """
-https://pandas.pydata.org/docs/reference/frame.html
+https://pandas.pydata.org/docs/index.html
 
+Series.unique()               Return unique values of Series object
+Series.nunique(dropna=True)   Return number of unique elements in the object
 
-DataFrame.index         The index (row labels) of the DataFrame
-DataFrame.columns       The column labels of the DataFrame
-DataFrame.dtypes        Return the dtypes in the DataFrame
-DataFrame.values        Return a Numpy representation of the DataFrame
-DataFrame.axes          [index, columns]
-DataFrame.ndim          Return dimensions, 1 for array, 2 for matrix
-DataFrame.size          len(index) * len(columns)
-DataFrame.shape         Return a tuple representing the dimensionality of the DataFrame
-DataFrame.empty         Indicator whether DataFrame is empty
+DataFrame.index               The index (row labels) of the DataFrame
+DataFrame.columns             The column labels of the DataFrame
+DataFrame.dtypes              Return the dtypes in the DataFrame
+DataFrame.values              Return a Numpy representation of the DataFrame
+DataFrame.axes                [index, columns]
+DataFrame.ndim                Return dimensions, 1 for array, 2 for matrix
+DataFrame.size                len(index) * len(columns)
+DataFrame.shape               Return a tuple representing the dimensionality of the DataFrame
+DataFrame.empty               Indicator whether DataFrame is empty
+DataFrame.value_counts()      Return a Series containing counts of unique rows in the DataFrame
 
-
-DataFrame.value_counts()
-  -> Return a Series containing counts of unique rows in the DataFrame
-
+DataFrame.nunique(axis=0, dropna=True)
+  -> Count distinct observations over requested axis
 DataFrame.drop_duplicates(subset=None, keep='first', inplace=False, ignore_index=False)
   -> Return DataFrame with duplicate rows removed
-
 DataFrame.astype(dtype, copy=True, errors='raise')
   -> Cast a pandas object to a specified dtype dtype.
-
 DataFrame.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
   -> Remove missing values
-
 DataFrame.fillna(value=None, method=None, axis=None, inplace=False, limit=None, downcast=None)
   -> Fill NA/NaN values using the specified method
-
 DataFrame.drop(labels=None, axis=0, index=None, columns=None, level=None, inplace=False, errors='raise')
   -> Drop specified labels from rows or columns
-
 DataFrame.merge(right, how='inner', on=None, left_on=None, right_on=None, left_index=False, right_index=False, sort=False, suffixes=('_x', '_y'), copy=True, indicator=False, validate=None)
   -> Merge DataFrame or named Series objects with a database-style join
-
 
 pandas.to_numeric(arg, errors='raise', downcast=None)
   -> Convert argument to a numeric type
   -> dtypedata type, or dict of column name -> data type
-
 pandas.merge(left, right, how='inner', on=None, left_on=None, right_on=None, left_index=False, right_index=False, sort=False, suffixes=('_x', '_y'), copy=True, indicator=False, validate=None)
   -> Merge DataFrame or named Series objects with a database-style join
 
