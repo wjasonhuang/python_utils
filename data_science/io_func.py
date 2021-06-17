fd077d8c9e30a8854d09719fff74a65a874fbae4
wjasonhuang <54944790+wjasonhuang@users.noreply.github.com>
2021-06-16 23:27:44 -0400

    Update and rename sample.py to io_func.py

---
+++ data_science/io_func.py
@@ -0,0 +1,10 @@
+import csv
+with open('albumlist.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    print(reader.fieldnames)
+    albums = [row for row in reader]
+    
+
+import pandas as pd
+xls = pd.ExcelFile('yelp.xlsx')
+df = xls.parse('yelp_data')
